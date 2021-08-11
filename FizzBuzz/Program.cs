18c565cd69a432559ddbccaa7d5d03f12402008c
Dragan Pepic <d.pepic@hotmail.com>
2021-08-11 18:47:35 +0200

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj = 1;
+            while (broj <= 100)
+            {
+                Console.Write(broj);
+                if (broj % 3 == 0)
+                {
+                    Console.Write(" Fizz");
+                }
+
+                if (broj % 5 == 0)
+                {
+                    Console.Write(" Buzz");
+                }
+
+                Console.WriteLine();
+                broj++;
+            }
+        }
+    }
+}
